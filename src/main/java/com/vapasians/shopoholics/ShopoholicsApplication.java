53a1d0d3f1e9ea82d13552b9c8c8fd47d7be4aeb
Sadaf Alam <sadaf.alam@Sadafs-MacBook-Pro.local>
2019-08-22 12:58:42 +0530

    Initial project structure with no code - barebone

---
+++ src/main/java/com/vapasians/shopoholics/ShopoholicsApplication.java
@@ -0,0 +1,13 @@
+package com.vapasians.shopoholics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopoholicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopoholicsApplication.class, args);
+    }
+
+}

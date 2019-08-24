0a24b85435314b2106b43242df9bbcf717a6d41b
Sadaf Alam <sadaf.alam@Sadafs-MacBook-Pro.local>
2019-08-24 11:49:10 +0530

    Sadaf : Iteration1/Story3 : User login page and redirect to home page post success : No validations

--- src/main/java/com/vapasians/shopoholics/controller/ProductController.java
+++ src/main/java/com/vapasians/shopoholics/controller/ProductController.java
@@ -1,29 +1,30 @@
 package com.vapasians.shopoholics.controller;
 
 import com.vapasians.shopoholics.model.Product;
+import com.vapasians.shopoholics.model.User;
 import com.vapasians.shopoholics.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
 public class ProductController {
-@Autowired
-   private ProductService productService;
 
-@GetMapping("/getProducts")
-    public String showProductList(Model model)
-{
-     model.addAttribute("products",productService.findAll());
-     return "home";
+    @Autowired
+    private ProductService productService;
 
-}
-
-
-
-
+    @GetMapping("/")
+    public String showProductList(Model model,RedirectAttributes redirectAttributes,HttpSession session)
+    {
+        System.out.println(session.getAttribute("loggedInUser"));
+        model.addAttribute("products",productService.findAll());
+        return "home";
+    }
 }
--- src/main/java/com/vapasians/shopoholics/controller/UserController.java
+++ src/main/java/com/vapasians/shopoholics/controller/UserController.java
@@ -1,33 +1,80 @@
 package com.vapasians.shopoholics.controller;
 
-import com.vapasians.shopoholics.Model.User;
 import com.vapasians.shopoholics.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import com.vapasians.shopoholics.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 @Controller
-    public class UserController {
+//@SessionAttributes("loggedInUserThisController")
+public class UserController {
+    @Autowired
+    UserService userService;
 
-        @Autowired
-        UserService userService;
+    @Autowired
+    ProductController productController;
 
-        @ModelAttribute("user")
-        public User getUser(){ return new User();};
+//    @ModelAttribute("loggedInUser")
+//    public User createLoggedInSessionObject()
+//    {
+//        return new User();
+//    }
 
-        @GetMapping(value="/")
-        public String signup(Model model){
-            System.out.println("in controller");
-           return "signup";
-        }
-
-
-        @PostMapping("/saveUser")
-        public String saveUser(User user)
+    @GetMapping("/login")
+    public ModelAndView showLoginPage(Model model,RedirectAttributes redirectAttributes, HttpSession session, @ModelAttribute("loggedInUserThisController") User loggedInUserThisController)
+    {
+        if(session.getAttribute("loggedInUser") != null)
         {
-            userService.saveUser(user);
-            return "home";
+            redirectAttributes.addFlashAttribute("loggedInUser",session.getAttribute("loggedInUser"));
+            return new ModelAndView("redirect:/");
         }
+        model.addAttribute("user",new User());
+        return new ModelAndView("/login");
+    }
+
+    @GetMapping(value="/signUp")
+    public String signup(Model model){
+        model.addAttribute("user",new User());
+        return "signup";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(User user)
+    {
+        userService.saveUser(user);
+        return "home";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView loginUser(@ModelAttribute("user") User user,RedirectAttributes redirectAttributes,HttpSession session,@ModelAttribute("loggedInUser") User loggedInUserThisController) {
+        if(session.getAttribute("loggedInUser") != null)
+        {
+            redirectAttributes.addFlashAttribute("loggedInUser",session.getAttribute("loggedInUser"));
+            return new ModelAndView("redirect:/");
+        }
+        else
+        {
+           Optional<User> validUserOrNull = userService.authenticateUser(user.getLoginName(), user.getLoginPwd());
+            if (!validUserOrNull.isPresent())
+                return new ModelAndView("/login");
+            else
+            {
+                //System.out.println(validUserOrNull.get());
+                session.setAttribute("loggedInUser",validUserOrNull.get());
+                redirectAttributes.addFlashAttribute("loggedInUser",validUserOrNull.get());
+                //System.out.println(session.getAttribute("theUser"));
+                return new ModelAndView("redirect:/");
+                //return productController.showProductList(model,(User)session.getAttribute("loggedInUserThisController"));
+            }
+        }
+    }
 }

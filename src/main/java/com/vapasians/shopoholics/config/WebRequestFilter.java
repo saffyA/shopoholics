3d6a9b3549805c97283e0448c40d6a5c34138173
Sadaf Alam <sadaf.alam@Sadafs-MacBook-Pro.local>
2019-08-29 07:52:27 +0530

    Sadaf : Added filter to intercept requests and determine if user is authorized, else redirect to home/login/adminhome as applicable

---
+++ src/main/java/com/vapasians/shopoholics/config/WebRequestFilter.java
@@ -0,0 +1,45 @@
+package com.vapasians.shopoholics.config;
+
+import com.vapasians.shopoholics.model.User;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Component
+public class WebRequestFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest)request).getSession();
+        String url = ((HttpServletRequest)request).getRequestURI();
+        System.out.println(url);
+        if(session.getAttribute("loggedInUser")!=null) {
+            System.out.println("We have a logged in user");
+            if(url.contains("signUp") || url.contains("login"))
+                ((HttpServletResponse)response).sendRedirect("/");
+            if(url.contains("admin"))
+            {
+                if(((User)session.getAttribute("loggedInUser")).getRole()!='A')
+                    ((HttpServletResponse)response).sendRedirect("/");
+            }
+            if(((User)session.getAttribute("loggedInUser")).getRole()=='A')
+            {
+                if(url.equals("/") || url.contains("cart") || url.contains("checkout") || url.contains("Ox`x`rder"))
+                if(((User)session.getAttribute("loggedInUser")).getRole()=='A')
+                    ((HttpServletResponse)response).sendRedirect("/admin");
+            }
+        }
+        else
+        {
+            if(url.contains("/admin") || url.contains("/checkout") || url.contains("/placeOrder") || url.contains("cart")) {
+                System.out.println("url starts with admin - not allowed as guest");
+                    ((HttpServletResponse)response).sendRedirect("/login");
+            }
+        }
+        chain.doFilter(request,response);
+    }
+}
